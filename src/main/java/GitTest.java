8a8d6912795efde622eb2264fefca0f1085a3c6f
rein <843097542@qq.com>
2023-07-24 09:43:04 +0800

    测试分支合并

---
+++ src/main/java/GitTest.java
@@ -0,0 +1,11 @@
+/**
+ * @ClassName GitTest
+ * @Author shuai
+ * @create 2023/7/24 9:41
+ * @Instruction
+ */
+public class GitTest {
+    public static void main(String[] args) {
+        System.out.println("hello,world");
+    }
+}
